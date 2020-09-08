f711f2ed28db568a944a672833e40abf370248d2
Jose Luis <jluisvazquez.jv@gmail.com>
2020-09-08 14:45:11 -0500

    ep 49 Pedir carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -9,6 +9,8 @@ let deck         = [];
 const tipos      = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
 
+
+// crear deck
 const clearDeck = () => {
 
     for( let i = 2; i <=10; i++ ) {
@@ -23,7 +25,6 @@ const clearDeck = () => {
         }
     }
 
-    console.log( deck );
     deck = _.shuffle( deck );
     console.log( deck );
     return deck;
@@ -31,3 +32,20 @@ const clearDeck = () => {
 } 
 
 clearDeck();
+
+//tomar carta
+const pedirCarta = () => {
+
+    if ( deck.length === 0 ) {
+        throw 'No hay mas cartas en el deck';
+    }
+
+
+    const carta = deck.pop();
+
+    console.log(deck);
+    console.log(carta); //carta que baja
+    return carta;
+}
+
+//pedirCarta();
